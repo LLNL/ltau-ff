f14b4b7ec115d6e771af7d07fac085ef3b528fe2
Joshua Vita <vita1@llnl.gov>
2024-10-30 15:41:39 -0700

    adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ltau_ff',
     version='0.0.1',
     packages=find_packages(),
-    # install_requires=['faiss-cpu']  # likely needs to be installed separately
+    install_requires=['faiss-cpu==1.8.0'],  # may need to install separately
     scripts=[
         'scripts/ltau-ff-nequip-descriptors',
         'scripts/ltau-ff-mace-descriptors',
