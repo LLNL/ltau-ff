938aa367b63e3d3f8cea33b0a854f1943274e4ec
Josh Vita <vita.joshua@gmail.com>
2025-07-10 16:53:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ltau_ff',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=['faiss==1.7.4'],  # may need to install separately
+    # install_requires=['faiss==1.7.4'],  # may need to install separately
     scripts=[
         'scripts/ltau-ff-nequip-descriptors',
         'scripts/ltau-ff-mace-descriptors',
